0d959cba9fe921604e6a3e99f46159a4c4aea18b
M. Bagher Abiat <zorofight94@gmail.com>
2020-07-27 17:50:30 +0430

    createstate function ☕

---
+++ src/createState.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { useContext, useStore } from './hooks';
+const createState = <T, U>(defaultState?: T, defaultDispatch?: U) => {
+	const stateContext = React.createContext<T | undefined>(defaultState);
+	const dispatchContext = React.createContext<U | undefined>(defaultDispatch);
+	return {
+		stateContext,
+		dispatchContext,
+		useStateContext: useContext.bind(null, stateContext),
+		useDispatchContext: useContext.bind(null, dispatchContext),
+		useStore: useStore.bind(null, stateContext, dispatchContext),
+		Provider: ({ state, dispatch, children }: { state: T; dispatch?: U; children?: React.ReactChildren }) => {
+			return (
+				<stateContext.Provider value={state}>
+					<dispatchContext.Provider value={dispatch}>{children}</dispatchContext.Provider>
+				</stateContext.Provider>
+			);
+		}
+	};
+};
+
+export default createState;
