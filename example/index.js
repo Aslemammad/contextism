0190bc3e50f33a9f9523bac16ffcf71af265ce14
M. Bagher Abiat <zorofight94@gmail.com>
2020-07-27 17:53:12 +0430

    Example project 🤙

---
+++ example/index.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { createState, useStore } from '../dist/index';
+
+const context = createState('s');
+
+const Div = () => {
+	const cont = context.useStateContext();
+	const [
+		state,
+		dispatch
+	] = context.useStore();
+
+	useEffect(() => {});
+
+	return (
+		<div className=''>
+			here <div className=''> {state}</div>
+		</div>
+	);
+};
+const App = () => {
+	const [
+		state,
+		dispatch
+	] = React.useState('23');
+
+	return (
+		<div className=''>
+			<context.Provider state={state} dispatch={dispatch}>
+				<Div />
+			</context.Provider>
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
