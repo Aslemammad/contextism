bcf854eaf02815007c524957d3e2d174a6827460
M. Bagher Abiat <zorofight94@gmail.com>
2020-07-27 17:57:22 +0430

    Delete public/

--- example/index.js
+++ example/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { createState, useStore } from '../dist/index';
+import { createState } from '../dist/index';
 
 const context = createState('s');
 
