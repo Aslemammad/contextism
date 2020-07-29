320f26febd595e04398bb78ee5b21ec4f1436546
M. Bagher Abiat <zorofight94@gmail.com>
2020-07-29 14:56:30 +0430

    ES5 compability

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
+import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 export default {
 	input    : 'src/index.tsx',
@@ -19,6 +20,7 @@ export default {
 	plugins  : [
 		typescript({
 			typescript : require('typescript')
-		})
+		}),
+		babel()
 	]
 };
